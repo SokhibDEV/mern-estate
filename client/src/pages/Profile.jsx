68d6de381299ade932704269b8b11bc769fd7a5f
Sokhib <amonovsokhib@gmail.com>
2024-04-28 23:23:35 +0500

    complate create listing page Ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { useEffect, useRef, useState } from "react";
+import {Link} from "react-router-dom" 
 import {
   getDownloadURL,
   getStorage,
@@ -120,9 +121,9 @@ export default function Profile() {
         dispatch(deleteUserFailure(data.message));
         return;
       }
-      dispatch(deleteUserSuccess(data))
+      dispatch(deleteUserSuccess(data));
     } catch (error) {
-      dispatch(deleteUserFailure(data.message))
+      dispatch(deleteUserFailure(data.message));
     }
   };
   return (
@@ -185,6 +186,7 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={'/create-listing'}>Create Listing</Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
